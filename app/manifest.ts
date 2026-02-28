c718390b9d519d9ea4f95d6585c6434bd18a27fb
Alda Julian <dev.alda.julian@gmail.com>
2026-03-01 00:25:39 +0700

    fix: fix manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,25 +2,25 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "Bila Libur",
-    short_name: "BilaLibur",
+    name: "Hari Libur",
+    short_name: "Hari Libur",
     description:
-      "Bila Libur adalah aplikasi yang membantu Anda mengetahui liburan di Indonesia.",
+      "Hari Libur adalah aplikasi yang membantu Anda mengetahui liburan di Indonesia.",
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#ffffff",
-    // icons: [
-    //   {
-    //     src: '/icon-192x192.png',
-    //     sizes: '192x192',
-    //     type: 'image/png',
-    //   },
-    //   {
-    //     src: '/icon-512x512.png',
-    //     sizes: '512x512',
-    //     type: 'image/png',
-    //   },
-    // ],
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
   };
 }
